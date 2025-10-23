379eeac25b08c607ef3ac3e15f597240dcb66784
OwenNyo <2301840@sit.singaporetech.edu.sg>
2025-10-23 13:05:30 +0800

    Update uploadImage.js

--- frontend/expense-tracker/src/utils/uploadImage.js
+++ frontend/expense-tracker/src/utils/uploadImage.js
@@ -3,7 +3,7 @@ import { API_PATHS } from "./apiPaths";
 import axiosInstance from "./axiosInstance";
 
 const uploadImage = async (imageFile) => {
-    const formData = new formData();
+    const formData = new FormData();
 
     // Append image file to form data
     formData.append('image', imageFile);
